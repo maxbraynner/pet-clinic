d80f8d0e117e55ca78215110fa69b158bd522e4f
Max Braynner <max.braynner@gmail.com>
2019-01-18 00:30:58 -0300

    Classe BaseEntity adicionada

--- pet-clinic-data/src/main/java/com/max/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/max/petclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.max.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
--- pet-clinic-data/src/main/java/com/max/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/max/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.max.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
