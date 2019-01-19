e62d2127d43d8f09ea96c85036fac40236f610e0
Max Braynner <max.braynner@gmail.com>
2019-01-18 23:45:08 -0300

    Pagina inicial de Vets adicionada

---
+++ pet-clinic-web/src/main/java/com/max/petclinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package com.max.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping("/vets")
+    public String listVets(){
+
+        return "vets/listVets";
+    }
+
+}
