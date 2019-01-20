31d01f9018ce7cf78254bf58864bdc113cf5d408
Max Braynner <max.braynner@gmail.com>
2019-01-20 11:36:09 -0300

    Carga nos serviços Map e pequenos ajustes

---
+++ pet-clinic-web/src/main/java/com/max/petclinic/bootstrap/DataInitializer.java
@@ -0,0 +1,57 @@
+package com.max.petclinic.bootstrap;
+
+import com.max.petclinic.model.Owner;
+import com.max.petclinic.model.Vet;
+import com.max.petclinic.services.OwnerService;
+import com.max.petclinic.services.VetService;
+import com.max.petclinic.services.map.OwnerServiceMap;
+import com.max.petclinic.services.map.VetServiceMap;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    public DataInitializer() {
+        this.ownerService = new OwnerServiceMap();
+        this.vetService = new VetServiceMap();
+    }
+
+    @Override
+    public void run(String... args) {
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("Michael");
+        owner1.setLastName("Weston");
+
+        ownerService.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glenanne");
+
+        ownerService.save(owner2);
+
+        System.out.println("Loaded Owners....");
+
+        Vet vet1 = new Vet();
+        vet1.setId(1L);
+        vet1.setFirstName("Sam");
+        vet1.setLastName("Axe");
+
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setId(2L);
+        vet2.setFirstName("Jessie");
+        vet2.setLastName("Porter");
+
+        vetService.save(vet1);
+
+        System.out.println("Loaded Vets....");
+    }
+}
